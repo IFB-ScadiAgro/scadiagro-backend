32fd2523b1d4686c657861660b2345dc4a98a533
Tassio Melo Linhares <tassio.melo45@gmail.com>
2025-11-25 15:13:12 -0300

    (feat:SCAD-0002) Add Fornecedor and Usuario classes

---
+++ src/Fornecedor.java
@@ -0,0 +1,23 @@
+public class Fornecedor extends Usuario {
+    private String nomeFantasia;
+    private String razaoSocial;
+    private String cpfCnpjFornecedor;
+    private String telefoneFornecedor;
+
+    public void cadastrarFornecedor() {
+
+    }
+
+    public void excluirFornecedor() {
+
+    }
+
+    public void alterarFornecedor() {
+
+    }
+
+    public Fornecedor getFornecedorById(int id) {
+        return null;
+    }
+
+}
---
+++ src/Usuario.java
@@ -0,0 +1,23 @@
+public class Usuario {
+    private String idUser;
+    private String mailUser;
+    private String password;
+    private String role;
+    private String tokenApi;
+
+    public void registerUser() {
+
+    }
+
+    public void deleteUser() {
+
+    }
+
+    public void updateUser() {
+
+    }
+
+    public User getUserByUsername(String username) {
+        return null;
+    }
+}
